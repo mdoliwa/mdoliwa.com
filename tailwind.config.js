1ef70af529f917a823b15f532b62901ace0df47c
Marcin Doliwa <mdoliwa@gmail.com>
2023-04-02 11:54:06 +0200

    Add inter font

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: []
+}
